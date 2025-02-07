d6681bbce48684e4c4db4e45b83da12b39970526
MuhammadAbsarSiddiqui <absar.sid359@gmail.com>
2025-02-07 20:27:51 +0500

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+// import { ThreeDCardDemo } from './components/ThreeDCardDemo'
+function App() {
+
+  return (
+    <>
+     <h1>hi my name is ....</h1>
+     <Button>Click me</Button>
+    
+    </>
+  )
+}
+
+export default App
