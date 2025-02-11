78b18017b416ff1d385fe7bbdc5cc0457369783c
MuhammadAbsarSiddiqui <absar.sid359@gmail.com>
2025-02-11 19:48:31 +0500

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
 
 import './App.css'
-import { Button } from "@/components/ui/button"
-// import { ThreeDCardDemo } from './components/ThreeDCardDemo'
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
+import React from 'react'
+
 function App() {
 
-  const btnRef = useRef(null);
-
-  useEffect(() => {
-    gsap.fromTo(
-      btnRef.current,
-      { opacity: 0, scale: 0.8 },
-      { opacity: 1, scale: 1, duration: 5, ease: "back.out(1.7)" }
-    );
-  }, []);
-
   return (
-    <Button ref={btnRef} variant="default">
-      GSAP Animated Button
-    </Button>
+ <div>hello world</div>
   );
   
 }
